b0cdc03d4d6744dcf62d09b3ccf6cb95a39a66a1
bob_eaton <none@none>
2009-08-18 23:19:54 +0000

    fixed a bug whereby we were adding "ConsultantNoteData"s rather than "CoachNoteData"s with "Add Note" in the Coaching pane. Adjusted the NetBibleFootnoteTooltip to display the footnote in HTML and Unicode, so the greek/hebrew comes out nicely. And now the footnote is a form that just hides itself. Add several Bible versions to the Sword viewer and support to add those via radio buttons

--- StoryEditor/NetBibleFootnoteTooltip.designer.cs
+++ StoryEditor/NetBibleFootnoteTooltip.designer.cs
@@ -28,52 +28,55 @@ namespace OneStoryProjectEditor
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.lblText = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// lblText
-			// 
-			this.lblText.AutoSize = true;
-			this.lblText.BackColor = System.Drawing.Color.Transparent;
-			this.lblText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblText.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lblText.Location = new System.Drawing.Point(1, 0);
-            this.lblText.MaximumSize = new System.Drawing.Size(400, 800);
-            this.lblText.MinimumSize = new System.Drawing.Size(50, 15);
-			this.lblText.Name = "lblText";
-			this.lblText.Padding = new System.Windows.Forms.Padding(5);
-            this.lblText.Size = new System.Drawing.Size(50, 25);
-			this.lblText.TabIndex = 2;
-			// 
-			// NetBibleFootnoteTooltip
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.AutoSize = true;
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.AllowWebBrowserDrop = false;
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(6, 6);
+            this.webBrowser.MaximumSize = new System.Drawing.Size(400, 800);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(50, 15);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(380, 294);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.webBrowser_PreviewKeyDown);
+            // 
+            // NetBibleFootnoteTooltip
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(368, 202);
-			this.ControlBox = false;
-			this.Controls.Add(this.lblText);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.MaximizeBox = false;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(392, 306);
+            this.Controls.Add(this.webBrowser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(400, 800);
-			this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(25, 0);
-			this.Name = "NetBibleFootnoteTooltip";
-			this.ShowIcon = false;
-			this.ShowInTaskbar = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-			this.TopMost = true;
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(50, 26);
+            this.Name = "NetBibleFootnoteTooltip";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.NetBibleFootnoteTooltip_FormClosing);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.NetBibleFootnoteTooltip_PreviewKeyDown);
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.WebBrowser webBrowser;
 
 
-	}
+
+    }
 }
