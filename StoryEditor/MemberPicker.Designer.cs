575f9e32ee847a1348716fa91a6b9744c8f7be86
Bob <none@none>
2011-03-30 21:46:52 -0500

    add the ability to merge two UNSs into a single record (and rewrite all references to the obsolete record)
    --HG--
    branch : CZversion

--- StoryEditor/MemberPicker.Designer.cs
+++ StoryEditor/MemberPicker.Designer.cs
@@ -37,6 +37,9 @@
             // 
             // listBoxUNSs
             // 
+            this.listBoxUNSs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.listBoxUNSs.FormattingEnabled = true;
             this.listBoxUNSs.Location = new System.Drawing.Point(12, 12);
             this.listBoxUNSs.Name = "listBoxUNSs";
