5cf4b18bb99aba0ea0f4cd7c5f597c3439e48818
Bob@StudioXPS-1340.gateway.2wire.net <Bob@StudioXPS-1340.gateway.2wire.net>
2011-12-25 12:53:34 -0600

    working on StoryBtHtml (adding html and js functions). Got placeholders working
    --HG--
    branch : Non-bib texts

---
+++ StoryEditor/js/StoryBtPlaceholders.js
@@ -0,0 +1,12 @@
+﻿function VernacularLanguageName() {{
+    return '{0}';
+}}
+function NationalBtLanguageName() {{
+    return '{1}';
+}}
+function InternationalBtLanguageName() {{
+    return '{2}';
+}}
+function FreeTranslationLanguageName() {{
+    return '{3}';
+}}
