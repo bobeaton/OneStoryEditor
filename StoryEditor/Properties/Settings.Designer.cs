70eac38c2e8f934af6309c5378b6f7119045b661
bobeaton <beaton@sheplers.com>
2018-01-10 07:47:27 -0600

    make autosave the default

--- StoryEditor/Properties/Settings.Designer.cs
+++ StoryEditor/Properties/Settings.Designer.cs
@@ -495,7 +495,7 @@ namespace OneStoryProjectEditor.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool DoAutoSaveSilently {
             get {
                 return ((bool)(this["DoAutoSaveSilently"]));
