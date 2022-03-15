76504aa2cafff3dc122bd28af54667a231e7f0c7
beaton <beaton@sheplers.com>
2022-03-15 14:50:03 +0545

    needed to use Insignia for bundle exe

--- build/buildBaseInstaller.bat
+++ build/buildBaseInstaller.bat
@@ -4,4 +4,17 @@ REM Command line arguments and defined properties.
 SET MsiFileName=%1
 SHIFT
 
-call signingProxy %MsiFileName%
+SET UseInsignia=%1
+SHIFT
+
+if [%UseInsignia%]==[] (
+	echo signing %MsiFileName%
+	call signingProxy %MsiFileName%
+) else (
+	echo signing bundle exe %MsiFileName%
+	@REM Sign the standard installer.
+	insignia -ib %MsiFileName% -o engine.exe
+	call signingProxy engine.exe
+	insignia -ab engine.exe %MsiFileName% -o %MsiFileName%
+	call signingProxy %MsiFileName%
+)
